a4cb96dd373a379c7f18d57158b95f2799bfa71f
j.sierrac <j.sierrac@ISIS2603S3-0077.sis.virtual.uniandes.edu.co>
2018-02-10 13:47:28 -0500

    Se agrego la clase CompraDTO y CompraDetailDTO

---
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/CompraDetailDTO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.ivanysusbambam.dtos;
+
+/**
+ *
+ * @author j.sierrac
+ */
+public class CompraDetailDTO extends CompraDTO {
+    
+//   protected AutomovilDTO  automovil;
+//   private  VendedorDTO vendedorEncargado;
+//   protected PuntoDeVentaDTO puntoDeVenta;
+//   private ClienteDTO cliente
+//
+//    public CompraDetailDTO()
+//    {
+//        
+//    }
+//           
+//    public AutomovilDTO getAutomovil() {
+//        return automovil;
+//    }
+//
+//    public void setAutomovil(AutomovilDTO automovil) {
+//        this.automovil = automovil;
+//    }
+//
+//    public VendedorDTO getVendedorEncargado() {
+//        return vendedorEncargado;
+//    }
+//
+//    public void setVendedorEncargado(VendedorDTO vendedorEncargado) {
+//        this.vendedorEncargado = vendedorEncargado;
+//    }
+//
+//    public PuntoDeVentaDTO getPuntoDeVenta() {
+//        return puntoDeVenta;
+//    }
+//
+//    public void setPuntoDeVenta(PuntoDeVentaDTO puntoDeVenta) {
+//        this.puntoDeVenta = puntoDeVenta;
+//    }
+//
+//    public ClienteDTO getCliente() {
+//        return cliente;
+//    }
+//
+//    public void setCliente(ClienteDTO cliente) {
+//        this.cliente = cliente;
+//    }
+    
+    
+    
+}
