c671d193b6e876196b709f4d5ed1aaef288f51df
if.garcia <if.garcia@ISIS2603S3-0063.sis.virtual.uniandes.edu.co>
2018-02-12 13:45:19 -0500

    general modifies

--- s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/resources/CalificacionTiendaResource.java
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/resources/CalificacionTiendaResource.java
@@ -9,17 +9,24 @@ import co.edu.uniandes.csw.ivanysusbambam.dtos.CalificacionTiendaDTO;
 import co.edu.uniandes.csw.ivanysusbambam.dtos.CalificacionTiendaDetailDTO;
 import co.edu.uniandes.csw.ivanysusbambam.exceptions.BusinessLogicException;
 import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  *
  * @author if.garcia
  */
+@Path("calificacionCarro")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class CalificacionTiendaResource {
     
      /**
@@ -33,12 +40,12 @@ public class CalificacionTiendaResource {
      * 200 OK Devuelve todas las calificaciones del cliente.</code> 
      * </pre>
      * 
-     * @param id cédula del cliente.
-     * @return JSONArray  con la información de todos los prospectos de compra.
+     * @param id identificador unico del cliente.
+     * @return JSONArray  con la información de todas las calificaciones del cliente.
      * @throws BusinessLogicException si el cliente con el id dado no existe.
      */
     @GET
-    public List<CalificacionTiendaDTO> getCalificacionesTienda(@PathParam("id") int id) throws BusinessLogicException{
+    public List<CalificacionTiendaDTO> getCalificacionesTienda(@PathParam("id") Long id) throws BusinessLogicException{
         return null;
     }
     
@@ -54,22 +61,22 @@ public class CalificacionTiendaResource {
      * </code> 
      * </pre>
      * @param id id del cliente.
-     * @param pid id de la compra.
+     * @param pid id de la calificacion.
      * @return JSON la calificacion de tienda buscada.
      * @throws BusinessLogicException si no existe el cliente con el id dado o la calificacion con el id dado..
      */
     @GET
     @Path("{pid: \\d+}")
-    public CalificacionTiendaDetailDTO getProspectoCompra(@PathParam("id") int id, @PathParam("pid") int pid) throws BusinessLogicException{
+    public CalificacionTiendaDetailDTO getCalificacionTienda(@PathParam("id") Long id, @PathParam("pid") Long pid) throws BusinessLogicException{
         return null;
     }
     
     /**
-     * PUT /api/clientes/(id)/prospectosCompra/(pid): Obtiene un prospecto de compra según su id.
+     * PUT /api/clientes/(id)/calificacionesTienda/(pid): Obtiene una calificacion de una tienda según su id.
      * <pre> 
      * Codigos de respuesta:
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
-     * 200 OK se actualizó el prospecto de compra.
+     * 200 OK se actualizó la calificacion.
      * </code> 
      * <code style="color: #c7254e; background-color: #f9f2f4;">
      * 404 Not Found no existe el cliente o prospecto de compra con el id dado.
