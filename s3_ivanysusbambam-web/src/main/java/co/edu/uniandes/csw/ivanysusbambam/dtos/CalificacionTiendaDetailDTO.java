82f9ce9b9f73338ca6f2319b520374797ac660da
if.garcia <if.garcia@ISIS2603S3-0063.sis.virtual.uniandes.edu.co>
2018-02-11 20:01:38 -0500

    calificacionTiendaDetailDTO

---
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/CalificacionTiendaDetailDTO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.ivanysusbambam.dtos;
+
+/**
+ *
+ * @author if.garcia
+ */
+public class CalificacionTiendaDetailDTO extends CalificacionTiendaDTO{
+    
+    /**
+     * Representa el cliente que ha puesto la calificacion
+     */
+    private ClienteDTO cliente;
+    
+    /**
+     * Asigna un cliente respectivo a la clasificacion
+     * @param cliente 
+     */
+    public void setCliente(ClienteDTO cliente){
+        this.cliente = cliente;
+    }
+    
+    /**
+     * Retorna el cliente respectivo a la calificacion
+     * @return cliente
+     */
+    public ClienteDTO getCliente(){
+        return cliente;
+    }
+}
