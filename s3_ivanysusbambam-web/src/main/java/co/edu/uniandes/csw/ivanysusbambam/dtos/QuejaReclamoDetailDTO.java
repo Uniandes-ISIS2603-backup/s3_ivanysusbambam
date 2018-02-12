02b45faceca782aebb1f7cc37248f5f2dd64038b
if.garcia <if.garcia@ISIS2603S3-0063.sis.virtual.uniandes.edu.co>
2018-02-11 23:43:45 -0500

    Queja reclamo DTO y Detail

---
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/QuejaReclamoDetailDTO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.ivanysusbambam.dtos;
+
+/**
+ *
+ * @author if.garcia
+ */
+public class QuejaReclamoDetailDTO {
+    
+    private ClienteDTO cliente;
+    
+    public ClienteDTO getCliente(){
+        return cliente;
+    }
+    
+    public void setCliente(ClienteDTO cliente)
+    {
+        this.cliente = cliente;
+    }
+}
