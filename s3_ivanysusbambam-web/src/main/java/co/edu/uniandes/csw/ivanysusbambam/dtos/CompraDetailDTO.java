d0d4626bb2fe2306743d7419564aa583af71e19f
j.sierrac <j.sierrac@ISIS2603S3-0077.sis.virtual.uniandes.edu.co>
2018-02-12 17:52:24 -0500

    Se arregla error en medioDePagoDTO

--- s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/CompraDetailDTO.java
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/CompraDetailDTO.java
@@ -6,7 +6,7 @@
 package co.edu.uniandes.csw.ivanysusbambam.dtos;
 
 /**
- *Objeto de transferencia de datos detallado del cliente. Hereda de ClienteDTO <br>
+ *Objeto de transferencia de datos detallado de la compra. Hereda de CompraDTO <br>
  * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
  * < Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
  * <pre>
