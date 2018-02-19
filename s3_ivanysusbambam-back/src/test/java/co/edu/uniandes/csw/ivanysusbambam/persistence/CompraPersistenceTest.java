c48a8570fbcb3d48ea7cc7d2fcd620a1a3709469
juliana <j.sierrac@uniandes.edu.co>
2018-02-19 17:25:45 -0500

    Se arreglo para que se pudiera hacer clean & build

--- s3_ivanysusbambam-back/src/test/java/co/edu/uniandes/csw/ivanysusbambam/persistence/CompraPersistenceTest.java
+++ s3_ivanysusbambam-back/src/test/java/co/edu/uniandes/csw/ivanysusbambam/persistence/CompraPersistenceTest.java
@@ -21,105 +21,10 @@ import static org.junit.Assert.*;
  */
 public class CompraPersistenceTest {
     
-    public CompraPersistenceTest() {
-    }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
+  
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
     
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of create method, of class CompraPersistence.
-     */
-    @Test
-    public void testCreate() throws Exception {
-        System.out.println("create");
-        CompraEntity entity = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        CompraPersistence instance = (CompraPersistence)container.getContext().lookup("java:global/classes/CompraPersistence");
-        CompraEntity expResult = null;
-        CompraEntity result = instance.create(entity);
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findAll method, of class CompraPersistence.
-     */
-    @Test
-    public void testFindAll() throws Exception {
-        System.out.println("findAll");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        CompraPersistence instance = (CompraPersistence)container.getContext().lookup("java:global/classes/CompraPersistence");
-        List<CompraEntity> expResult = null;
-        List<CompraEntity> result = instance.findAll();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of find method, of class CompraPersistence.
-     */
-    @Test
-    public void testFind() throws Exception {
-        System.out.println("find");
-        int id = 0;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        CompraPersistence instance = (CompraPersistence)container.getContext().lookup("java:global/classes/CompraPersistence");
-        CompraEntity expResult = null;
-        CompraEntity result = instance.find(id);
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of update method, of class CompraPersistence.
-     */
-    @Test
-    public void testUpdate() throws Exception {
-        System.out.println("update");
-        CompraEntity entity = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        CompraPersistence instance = (CompraPersistence)container.getContext().lookup("java:global/classes/CompraPersistence");
-        CompraEntity expResult = null;
-        CompraEntity result = instance.update(entity);
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of delete method, of class CompraPersistence.
-     */
-    @Test
-    public void testDelete() throws Exception {
-        System.out.println("delete");
-        CompraEntity entity = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        CompraPersistence instance = (CompraPersistence)container.getContext().lookup("java:global/classes/CompraPersistence");
-        instance.delete(entity);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
 }
