c600a92565072ee6e14c7cf8527d31db1cfb2d8f
a.bravo <a.bravo@ISIS2603S3-0089.sis.virtual.uniandes.edu.co>
2018-02-19 19:41:59 -0500

    automovilcosas

--- s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/AutomovilDTO.java
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/AutomovilDTO.java
@@ -6,10 +6,37 @@
 package co.edu.uniandes.csw.ivanysusbambam.dtos;
 
 import co.edu.uniandes.csw.ivanysusbambam.entities.AutomovilEntity;
-import java.util.Calendar;
 
 /**
+ * AutomovilDTO Objeto de transferencia de datos de Automoviles. Los DTO contienen las
+ * represnetaciones de los JSON que se transfieren entre el cliente y el
+ * servidor.
+ * 
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "color": string,
+ *      "anio: int,
+ *      "placa": string,
+ *      "chasis": int,
+ *      "fechaListado": string,
+ *      "valorListado": double
+ *   }
+ * </pre>
+ * Por ejemplo un automovil se representa asi:<br>
+ * 
+ * <pre>
+ * 
+ *   {
+ *      "color": "azul",
+ *      "anio": 2001,
+ *      "placa": "XYZ123",
+ *      "chasis": 1,
+ *      "fechaListado": "11/02/2018",
+ *      "valorListado": 5000000
+ *   }
  *
+ * </pre>
  * @author a.bravo
  */
 public class AutomovilDTO {
@@ -17,11 +44,20 @@ public class AutomovilDTO {
     private int anio;
     private String placa;
     private int chasis;
-    private Calendar fechaListado;
+    private String fechaListado;
     private double valorListado;
     
+    /**
+     * Constructor por defecto
+     */
     public AutomovilDTO(){
     }
+    /**
+     * Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
+     * la entidad que viene de argumento.
+     *
+     * @param auto: Es la entidad que se va a convertir a DTO
+     */
     public AutomovilDTO(AutomovilEntity auto){
         this.color = auto.getColor();
         this.anio = auto.getAnio();
@@ -31,40 +67,76 @@ public class AutomovilDTO {
         this.valorListado = auto.getValorListado();
     }
     
+    /**
+     * @return El ID de la ciudad
+     */
     public String getColor(){
         return color;
     }
+    /**
+     * @return El anio del automovil
+     */
     public int getAnio(){
         return anio;
     }
+    /**
+     * @return La placa del automovil
+     */
     public String getPlaca(){
         return placa;
     }
+    /**
+     * @return El chasis del automovil
+     */
     public int getChasis(){
         return chasis;
     }
-    public Calendar getFechaListado(){
+    /**
+     * @return La fecha de listado del automovil
+     */
+    public String getFechaListado(){
         return fechaListado;
     }
+    /**
+     * @return El valor del listado del automovil
+     */
     public double getValorListado(){
         return valorListado;
     }
     
+    /**
+     * @param color El nuevo color del automovil
+     */
     public void setColor(String color){
         this.color = color;
     }
+    /**
+     * @param anio El nuevo anio del automovil
+     */
     public void setAnio(int anio){
         this.anio = anio;
     }
+    /**
+     * @param placa La nueva placa del automovil
+     */
     public void setPlaca(String placa){
         this.placa = placa;
     }
+    /**
+     * @param chasis El nuevo chasis del automovil
+     */
     public void setChasis(int chasis){
         this.chasis = chasis;
     }
-    public void setFechaListado(Calendar fechaListado){
+    /**
+     * @param fechaListado La nueva fecha de listado del automovil
+     */
+    public void setFechaListado(String fechaListado){
         this.fechaListado = fechaListado;
     }
+    /**
+     * @param valorListado El nuevo valor de listado del automovil
+     */
     public void setValorListado(double valorListado){
         this.valorListado = valorListado;
     }
--- s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/AutomovilDetailDTO.java
+++ s3_ivanysusbambam-web/src/main/java/co/edu/uniandes/csw/ivanysusbambam/dtos/AutomovilDetailDTO.java
@@ -8,15 +8,30 @@ package co.edu.uniandes.csw.ivanysusbambam.dtos;
 import co.edu.uniandes.csw.ivanysusbambam.entities.AutomovilEntity;
 
 /**
- *
+ * Clase que extiende de {@link AutomovilDTO} para manejar la transformacion entre
+ * los objetos JSON y las Entidades de la base de datos. Para conocer el
+ * contenido del automovil vaya a la documentacion de {@link AutomovilDTO}
  * @author a.bravo
  */
 public class AutomovilDetailDTO extends AutomovilDTO{
+    /**
+     * Constructor por defecto
+     */
     public AutomovilDetailDTO(){
     }
+    /**
+     * Constructor para transformar un Entity a un DTO
+     *
+     * @param entity La entidad del automovil a partir de la cual se construye el objeto
+     */
     public AutomovilDetailDTO(AutomovilEntity entity){
         super(entity);
     }
+    /**
+     * Transformar un DTO a un Entity
+     *
+     * @return  La entidad construida a partir del DTO.
+     */
     @Override
     public AutomovilEntity toEntity(){
         AutomovilEntity autoE = super.toEntity();
