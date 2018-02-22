4365bc6a51b31724dd967be0f5a6148e94198302
a.bravo <a.bravo@ISIS2603S3-0089.sis.virtual.uniandes.edu.co>
2018-02-21 20:18:35 -0500

    reparacion parcial automovil entity

--- s3_ivanysusbambam-back/src/main/java/co/edu/uniandes/csw/ivanysusbambam/entities/AutomovilEntity.java
+++ s3_ivanysusbambam-back/src/main/java/co/edu/uniandes/csw/ivanysusbambam/entities/AutomovilEntity.java
@@ -29,40 +29,76 @@ public class AutomovilEntity extends BaseEntity implements Serializable{
     
     private double valorListado;
     
+    /**
+     * @return color del auto
+     */
     public String getColor(){
         return color;
     }
+    /**
+     * @return anio del auto
+     */
     public int getAnio(){
         return anio;
     }
+    /**
+     * @return placa del auto
+     */
     public String getPlaca(){
         return placa;
     }
+    /**
+     * @return chasis del auto
+     */
     public int getChasis(){
         return chasis;
     }
+    /**
+     * @return fecha de listado del auto
+     */
     public Calendar getFechaListado(){
         return fechaListado;
     }
+    /**
+     * @return valor del listado del auto
+     */
     public double getValorListado(){
         return valorListado;
     }
     
+    /**
+     * @param color color a setear
+     */
     public void setColor(String color){
         this.color = color;
     }
+    /**
+     * @param anio anio a setear
+     */
     public void setAnio(int anio){
         this.anio = anio;
     }
+    /**
+     * @param placa placa a setear
+     */
     public void setPlaca(String placa){
         this.placa = placa;
     }
+    /**
+     * @param chasis chasis a setear
+     */
     public void setChasis(int chasis){
         this.chasis = chasis;
     }
+    /**
+     * @param fechaListado fecha del listado a setear
+     */
     public void setFechaListado(Calendar fechaListado){
         this.fechaListado = fechaListado;
     }
+    /**
+     * @param valorListado valor del listado a setear
+     */
     public void setValorListado(double valorListado){
         this.valorListado = valorListado;
     }
