0a25c36c6f9877fc4ef4e3964e96c4ad8dbf6103
Joseph Ortíz <js.ortiz@uniandes.edu.co>
2018-05-04 23:21:55 -0500

    tratando de arreglar el perfil y añadiendo cosas a los roles

--- s3_ivanysusbambam-web/src/main/webapp/src/modules/login/loginCtrl.js
+++ s3_ivanysusbambam-web/src/main/webapp/src/modules/login/loginCtrl.js
@@ -8,19 +8,39 @@
                                 
     function($scope, $http, $state,$rootScope){
       
-      
-      
     $scope.user = {};
     $scope.data = {};
-        $http.get('api/clientes').then(function (response)     {
+        $http.get('data/usuarios.json').then(function (response)     {
                 $scope.users = response.data;
             });
         
+        $rootScope.usuario = false;
+        $rootScope.vendedor = false; 
+        $rootScope.administrador = false;
+        
+        $rootScope.esUsuario = function()
+        {
+            $rootScope.usuario=true;
+            $rootScope.vendedor=false;
+            $rootScope.administrador=false;
+        }
+        $rootScope.esAdministrador = function()
+        {
+            $rootScope.usuario=false;
+            $rootScope.vendedor=false;
+            $rootScope.administrador=true;
+        }
+        $rootScope.esVendedor = function()
+        {
+            $rootScope.usuario=false;
+            $rootScope.vendedor=true;
+            $rootScope.administrador=false;
+        }
         $scope.autenticar = function () {
                 var flag = false;
              $http.post('api/login',$scope.data).then(function(response){
                 for (var item in $scope.users) {
-                    if ($scope.users[item].user === $scope.data.username && $scope.users[item].password === $scope.data.password && $scope.users[item].rol === $scope.data.rol) {
+                    if ($scope.users[item].user == $scope.data.username && $scope.users[item].password == $scope.data.password && $scope.users[item].rol == $scope.data.rol) {
                         flag = true;
                         $scope.user = $scope.users[item];
                         /*
@@ -40,6 +60,7 @@
                     $rootScope.currentUser = $scope.user.name; 
                 }
                 });
+            
             };
         }
     ]);
