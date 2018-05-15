471f7d6a6245ecc97885bfe7085b8940d8fa59de
juliana <j.sierrac@uniandes.edu.co>
2018-05-14 22:54:00 -0500

    Se arregla el html de comprar para que funcione con el back

--- s3_ivanysusbambam-web/src/main/webapp/src/modules/automovil/automovil.ctrl.js
+++ s3_ivanysusbambam-web/src/main/webapp/src/modules/automovil/automovil.ctrl.js
@@ -1,8 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 
 (function(ng){
@@ -11,7 +7,7 @@
     
     mod.constant("automovilContext", "api/automoviles");
     
-    mod.controller("automovilGetAllCtrl",["$scope","$http","$rootScope","automovilContext", function($scope, $rootScope, $http, automovilContext){
+    mod.controller("automovilGetAllCtrl",["$scope","$http","$rootScope","automovilContext", function($scope,  $http, $rootScope,automovilContext){
       
             console.log($rootScope.cliente);
             
