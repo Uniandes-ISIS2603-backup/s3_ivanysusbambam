9c9c32f90e411fca923ccb04e3fb3c68cf9157c0
juliana <j.sierrac@uniandes.edu.co>
2018-05-14 23:16:05 -0500

    Pequeños cambios visuales

--- s3_ivanysusbambam-web/src/main/webapp/src/modules/automovil/automovil.ctrl.js
+++ s3_ivanysusbambam-web/src/main/webapp/src/modules/automovil/automovil.ctrl.js
@@ -5,7 +5,7 @@
 
     var mod = ng.module("automovilModule");
     
-    mod.constant("automovilContext", "api/automoviles");
+    mod.constant("automovilContext", "api/automoviles/search?");
     
     mod.controller("automovilGetAllCtrl",["$scope","$http","$rootScope","automovilContext", function($scope,  $http, $rootScope,automovilContext){
       
